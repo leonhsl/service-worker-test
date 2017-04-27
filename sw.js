541b0e684a12d7a40dbea67dc97a36e4419c82a0
Han Leon <leon.han@intel.com>
2017-04-27 04:26:35 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 self.addEventListener('fetch', function(event) {
   console.log('url clicked: ', event.request.url);
+  console.log('clients.matchAll(): ' + clients.matchAll());
   if (/\.jpg$/.test(event.request.url)) {
     event.respondWith(
       fetch('https://www.google.co.uk/logos/doodles/2014/60th-anniversary-of-the-unveiling-of-the-first-routemaster-bus-4922931108904960.3-hp.gif', {
