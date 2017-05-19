edd6f99248975517f46231ae7f90c13f715ad696
Han Leon <leon.han@intel.com>
2017-05-19 04:04:39 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,6 +3,7 @@ self.addEventListener('fetch', function(event) {
   console.log('clients.matchAll() started');
   clients.matchAll().then((clients) => { console.log('Got clients[0].type: ' + clients[0].type); });
   if (/\.jpg$/.test(event.request.url)) {
+    console.log('fetch jpg: ' + event.request.url);
     event.respondWith(
       fetch('https://www.google.co.uk/logos/doodles/2014/60th-anniversary-of-the-unveiling-of-the-first-routemaster-bus-4922931108904960.3-hp.gif', {
         mode: 'no-cors'
